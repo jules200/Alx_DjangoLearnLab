4a2fbee14cf34a3d6fec2ce2cb177abb56faa685
jules200 <muhayejules@gmail.com>
2024-08-14 00:43:05 +0200

    first commit

--- 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/admin.py
+++ 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from .models import Book
 # Register your models here.
-@admin_register(Book)
+@admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('list_filter', 'author', 'publication_year')
     list_filter = ('author', 'publication_year')
     search_fields = ('title', 'author')
-    
---
+++ django-models/relationship_app/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+
+# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        pass
+
+    class Meta:
+        db_table = ''
+        managed = True
+        verbose_name = 'Author'
+        verbose_name_plural = 'Authors'
+
+class Book(models.Model):
+    title = models.CharField(max_length=50)
+    Author = models.ForeignKey(Author, null=False, on_delete=models.CASCADE)
+    def __str__(self):
+        pass
+
+    class Meta:
+        db_table = ''
+        managed = True
+        verbose_name = 'Book'
+        verbose_name_plural = 'Books'
+        
+class Library(models.Model):
+    name = models.CharField(max_length=50)
+    books = models.ManyToManyField(Book, null=False)
+    def __str__(self):
+        pass
+
+    class Meta:
+        db_table = ''
+        managed = True
+        verbose_name = 'Library'
+        verbose_name_plural = 'Librarys'
+        
+class librarian(models.Model):
+    name = models.CharField(max_length=50)
+    library = models.OneToOneField(Library, null=False,  on_delete=models.CASCADE)
