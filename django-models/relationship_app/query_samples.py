1f76c4739a6aed010b24f79cf2c2126150ffa52b
jules200 <muhayejules@gmail.com>
2024-08-17 11:50:53 +0200

    first commit

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -2,6 +2,7 @@ from .models import *
 
 author= Author.objects.get(name=author_name)
 books = Books.objects.filter(author=author)
-librarian=Library.objects.get(name=library_name)
+library=Library.objects.get(name=library_name)
+librarian=Librarian.objects.get(library=library_name)
 
 books.all()
