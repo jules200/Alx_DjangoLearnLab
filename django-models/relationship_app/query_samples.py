3476ca83b0a2235469838374e00cb15db8913404
jules200 <muhayejules@gmail.com>
2024-08-17 11:38:18 +0200

    first commit

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -2,4 +2,6 @@ from .models import *
 
 author= Author.objects.get(name="jules")
 books = Books.objects.all()
-librarian=Librarian.objects.all()
+librarian=Librarian.objects.get(name=library_name)
+
+books.all()
