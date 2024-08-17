490ae0d48090c1604c770f3dc3a3cd7e27f66c55
jules200 <muhayejules@gmail.com>
2024-08-17 16:26:35 +0200

    first commit

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,5 +1,6 @@
-from .import views
+from .views import list_books
 
 urlspattern=[
     path('bookslist', views.bookslist, name='bookslist'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library-detail'),
 ]
