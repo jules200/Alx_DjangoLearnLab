0977c85ac314b885e525a76302319e16f9d4a20a
jules200 <muhayejules@gmail.com>
2024-08-19 17:02:49 +0200

    first commit

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,50 +1,27 @@
-from django.views.generic.detail import DetailView
-from django.contrib.auth import login, logout
-from .models import Library
-from .models import Book
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.views import LoginView, LogoutView
-from django.shortcuts import render, redirect
-from django.urls import reverse_lazy
-from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import render
 
-# Create your views here.
+# Helper functions to check the role of the user
 def is_admin(user):
     return user.userprofile.role == 'Admin'
 
-@login_required
-@user_passes_test(is_librarian)
-def bookslist(request):
-    books = Book.objects.all()
-    
-    return render(request, "relationship_app/list_books.html")
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
 
-@login_required
-@user_passes_test(is_librarian)
-class LibraryDetailView(DetailView):
-    model = Library
-    template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'
+def is_member(user):
+    return user.userprofile.role == 'Member'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['books'] = self.object.books.all()
-        return context
+# Admin view
 @user_passes_test(is_admin)
-def register(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('book-list')
-    else:
-        form = UserCreationForm()
-    return render(request, 'relationship_app/register.html', {'form': form})
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
 
-class LoginView(LoginView):
-    template_name = 'relationship_app/login.html'
+# Librarian view
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
 
-class LogoutView(LogoutView):
-    template_name = 'relationship_app/logout.html'
-    next_page = reverse_lazy('login')
+# Member view
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
