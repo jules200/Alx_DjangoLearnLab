5fad1515321eac98a452a1a7f8ddccfe69c3cdd0
jules200 <muhayejules@gmail.com>
2024-08-19 17:07:59 +0200

    first commit

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,27 +1,50 @@
-from django.contrib.auth.decorators import user_passes_test
-from django.shortcuts import render
+from django.views.generic.detail import DetailView
+from django.contrib.auth import login, logout
+from .models import Library
+from .models import Book
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView, LogoutView
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required, user_passes_test
 
-# Helper functions to check the role of the user
-def is_admin(user):
+# Create your views here.
+def Admin(user):
     return user.userprofile.role == 'Admin'
 
-def is_librarian(user):
-    return user.userprofile.role == 'Librarian'
-
-def is_member(user):
-    return user.userprofile.role == 'Member'
-
-# Admin view
-@user_passes_test(is_admin)
-def admin_view(request):
-    return render(request, 'relationship_app/admin_view.html')
-
-# Librarian view
+@login_required
 @user_passes_test(is_librarian)
-def librarian_view(request):
-    return render(request, 'relationship_app/librarian_view.html')
+def bookslist(request):
+    books = Book.objects.all()
+    
+    return render(request, "relationship_app/list_books.html")
 
-# Member view
-@user_passes_test(is_member)
-def member_view(request):
-    return render(request, 'relationship_app/member_view.html')
+@login_required
+@user_passes_test(is_librarian)
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['books'] = self.object.books.all()
+        return context
+@user_passes_test(is_admin)
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('book-list')
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
+
+class LoginView(LoginView):
+    template_name = 'relationship_app/login.html'
+
+class LogoutView(LogoutView):
+    template_name = 'relationship_app/logout.html'
+    next_page = reverse_lazy('login')
