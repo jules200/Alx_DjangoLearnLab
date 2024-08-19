55e56c3febdd746135cfee3e09fbeb16d7f2ac0c
jules200 <muhayejules@gmail.com>
2024-08-19 16:43:35 +0200

    first commit

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -2,6 +2,19 @@ from django.db import models
 
 # Create your models here.
 
+ROLE_CHOICES = [
+    ('Admin', 'Admin'),
+    ('Librarian', 'Librarian'),
+    ('Member', 'Member')
+]
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
+
 class Author(models.Model):
     name = models.CharField(max_length=100)
     def __str__(self):
