a722fdd9fc782625cf2509459d6642857a431bd7
jules200 <muhayejules@gmail.com>
2024-08-20 19:37:46 +0200

    first commit

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
-from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth.decorators import permission_required, login_required, user_passes_test
 
 # Create your views here.
 def role_check(user, role):
