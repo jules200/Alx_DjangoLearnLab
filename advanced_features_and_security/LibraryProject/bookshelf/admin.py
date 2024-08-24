6bfb8a5345243d56c40d0f34989cedce6f5b19ba
jules200 <muhayejules@gmail.com>
2024-08-24 16:14:32 +0200

    first commit

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Book
 # Register your models here.
-@admin.site.register(CustomUser)
+@admin.site.register(CustomUser, CustomUserAdmin)
 class AdminCustomUser(admin.ModelAdmin):
     pass
 @admin.register(Book)
