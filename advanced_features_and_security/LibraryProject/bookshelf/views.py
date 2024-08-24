9cf6ec875de8ff741365eb8bd9753a688949d321
jules200 <muhayejules@gmail.com>
2024-08-24 18:08:21 +0200

    first commit

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
+from .forms import ExampleForm
 
 # Create your views here.
 @permission_required("bookshelf.can_insert", raise_exception=True)
