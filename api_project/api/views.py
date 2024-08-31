61a0d65c3f7bafcac3f00983b658c68063570ea9
jules200 <muhayejules@gmail.com>
2024-08-31 19:04:45 +0200

    first commit

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-from rest_framework.generics.ListAPIView import ListAPIView
+from rest_framework.generics import ListAPIView
 from .models import Book
 from .serializers import BookSerializer
-
+from rest_framework import viewsets
+from .models import Book
+# generics.ListAPIView
 class BookList(ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
