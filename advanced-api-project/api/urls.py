69445d6bb5177c78142192c2e7d16a3affbd3553
jules200 <muhayejules@gmail.com>
2024-09-08 13:33:26 +0200

    first commit

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('books/create/', BookCreateView.as_view(), name='book-create'),
     path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
     path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
+    path("books/update", "books/delete"),
 ]
