27dfa13ef1083fca5ce6b1b0f7a707028e2fb541
jules200 <muhayejules@gmail.com>
2024-09-21 09:55:46 +0200

    first commit

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -12,4 +12,7 @@ urlpatterns = [
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-edit'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('posts/<int:post_id>/comments/new/', add_comment, name='comment-add'),
+    path('comments/<int:pk>/edit/', CommentUpdateView.as_view(), name='comment-edit'),
+    path('comments/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
 ]
