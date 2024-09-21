f0f91f3084eafdb8ea80aa77c40d856cccf8c213
jules200 <muhayejules@gmail.com>
2024-09-21 09:57:24 +0200

    first commit

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -92,7 +92,7 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
         return self.request.user == post.author
     
 @login_required
-def add_comment(request, post_id):
+def CommentCreateView(request, post_id):
     post = get_object_or_404(Post, id=post_id)
     if request.method == 'POST':
         form = CommentForm(request.POST)
