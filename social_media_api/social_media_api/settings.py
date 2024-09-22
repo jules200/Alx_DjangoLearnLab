3558c87efaabf415fbd3a09601c00fb5a14a5257
jules200 <muhayejules@gmail.com>
2024-09-22 16:15:45 +0200

    first commit

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -91,10 +91,22 @@ WSGI_APPLICATION = 'social_media_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'your_db_name',
+        'USER': 'your_db_user',
+        'PASSWORD': 'your_password',
+        'HOST': 'your_db_host',
+        'PORT': 'your_db_port',
     }
 }
 
