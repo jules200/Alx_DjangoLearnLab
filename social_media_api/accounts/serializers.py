5bb73f28dd8c61f239ede964927f9005ba408957
jules200 <muhayejules@gmail.com>
2024-09-22 13:03:37 +0200

    first commit

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
 
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
